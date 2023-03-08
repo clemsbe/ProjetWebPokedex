079bab3304e92c3448ff542661dafa619bc2a12d
clemsbe <110553572+clemsbe@users.noreply.github.com>
2023-03-08 10:36:57 +0100

    Service done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent implements OnInit{
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
+export class AppComponent{
 
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string) {
-    const id = +pokemonId;
-    const pokemon : Pokemon|undefined  = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
-   
-   if(pokemon){
-        console.log(`Vous avez cliqué sur le pokemon ${this.pokemonList[id].name}`);
-      this.pokemonSelected = pokemon;
-   } else {
-    console.log(`Vous avez demandé un pokemon qui n'existe pas.`);
-    this.pokemonSelected = pokemon;
-
-   }
-  }
 
 }
